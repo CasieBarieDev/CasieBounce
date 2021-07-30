5ec029fd1dcf7544423a59e6d0dfd2dcdec0f73d
casbarnard <casiebarie@gmail.com>
2021-07-30 21:10:33 +0200

    4.2

--- CasieBounce/src/me/casiebarie/casiebounce/CBTabCompleter.java
+++ CasieBounce/src/me/casiebarie/casiebounce/CBTabCompleter.java
@@ -14,7 +14,7 @@ public class CBTabCompleter implements TabCompleter{
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 		java.util.List<String> completions = new ArrayList<>();
 		if(cmd.getName().equalsIgnoreCase("CB")) {
-			if(args.length == 1) {
+			if(args.length == 1 && sender.hasPermission("CB.admin")) {
 				if(!args[0].equals("")) {
 					for(complete modes : complete.values()) {
 						if(modes.name().toUpperCase().startsWith(args[0].toUpperCase())) {completions.add(modes.name());}	
